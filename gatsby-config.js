a98786856e3247fad23cae61515fd011c347605b
Daywatch <1038999405@qq.com>
2021-04-07 09:45:15 +0800

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -112,13 +112,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
+        name: `文西的博客`,
+        short_name: `文西的博客`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/avatar.JPG`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
